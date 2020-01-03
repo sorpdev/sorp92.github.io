505b01d15a564b3f0cb61589897e6ce68d61d7be
Sorp <botsorp@gmail.com>
2020-01-03 15:29:23 +0100

    Update script.js
    HTTPS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+if (location.protocol != 'https:') location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems, {});
