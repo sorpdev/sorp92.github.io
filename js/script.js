f2bee09de14fb4b312a78e60bb25de96ff2aa956
Sorp <botsorp@gmail.com>
2020-01-16 20:33:05 +0100

    Added Jekyll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-if (location.protocol != 'https:') location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+if (location.protocol != 'https:' && location.hostname != "localhost") location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tooltipped');
