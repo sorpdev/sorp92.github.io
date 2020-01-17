9711a28d9e2e13ff173656b6076cab62359b48ff
Sorp <botsorp@gmail.com>
2020-01-17 07:05:06 +0100

    Fixed schedule timings and favicon generator

--- favicon-gen.js
+++ favicon-gen.js
@@ -2,63 +2,91 @@ const fs = require("fs");
 const pngToIco = require("png-to-ico");
 const filecompare = require('filecompare');
 const exec = require('child_process').exec;
+const sharp = require('sharp');
+
+function commitChanges() {
+    console.log("New favicon.ico detected. Commiting changes...");
+
+    exec("git add favicon.ico", (error, out, err) => {
+        console.log("git add favicon.ico");
+        if (error) {
+            console.log(out);
+            console.log(err);
+            throw error;
+        } else {
+            exec("git commit -m \"Update favicon.ico\"", (error, out, err) => {
+                console.log("git commit -m \"Update favicon.ico\"");
+
+                if (error) {
+                    console.log(out);
+                    console.log(err);
+                    throw error;
+                } else {
+                    console.log("Done.");
+                }
+
+            });
+        }
+    });
+}
+
 
 console.log("Checking for favicon.png...");
 
 if (fs.existsSync("favicon.png")) {
 
-    console.log("Converting favicon.png to favicon.ico");
+    console.log("Scaling favicon.png to 64x64...");
 
-    pngToIco("favicon.png").then(buf => {
+    sharp("favicon.png").resize(64, 64).toFile("_favicon.png", (error, info) => {
 
-        console.log("Removing old favicon.ico...");
+        if (error) {
+            throw error;
+        } else {
 
-        fs.copyFileSync("favicon.ico", "old-favicon.ico");
-        fs.unlinkSync("favicon.ico");
+            console.log("Scaled file: _favicon.png");
 
-        console.log("Writing new favicon.ico...")
+            console.log("Converting _favicon.png to favicon.ico");
 
-        fs.writeFileSync("favicon.ico", buf);
+            pngToIco("_favicon.png").then(buf => {
 
-        console.log("Done. Comparing...");
+                if (fs.existsSync("favicon.ico")) {
+                    console.log("Removing old favicon.ico...");
 
-        filecompare("favicon.ico", "old-favicon.ico", (equal) => {
+                    fs.copyFileSync("favicon.ico", "old-favicon.ico");
+                    fs.unlinkSync("favicon.ico");
+                }
 
-            if (equal) {
-                console.log("No change detected.");
-            } else {
-                console.log("New favicon.ico detected. Commiting changes...");
+                console.log("Writing new favicon.ico...")
 
-                exec("git add favicon.ico", (error, out, err) => {
-                    console.log("git add favicon.ico");
-                    if (error) {
-                        console.error(error);
-                        console.log(out);
-                        console.log(err);
-                    } else {
-                        exec("git commit -m \"Update favicon.ico\"", (error, out, err) => {
-                            console.log("git commit");
+                fs.writeFileSync("favicon.ico", buf);
 
-                            if (error) {
-                                console.error(error);
-                                console.log(out);
-                                console.log(err);
-                            } else {
-                                console.log("Done.");
-                            }
+                console.log("Done. Comparing...");
 
-                        });
-                    }
-                });
-            }
+                if (fs.existsSync("old-favicon.ico")) {
+                    filecompare("favicon.ico", "old-favicon.ico", (equal) => {
 
-            console.log("Cleaning up...");
+                        if (equal) {
+                            console.log("No change detected.");
+                        } else {
+                            commitChanges();
+                        }
 
-            fs.unlinkSync("old-favicon.ico");
+                    });
+                } else {
+                    commitChanges();
+                }
 
-        });
+                console.log("Cleaning up...");
 
-    }).catch(console.error);
+                if(fs.existsSync("favicon.png")) fs.unlinkSync("favicon.png");
+                if(fs.existsSync("_favicon.png")) fs.unlinkSync("_favicon.png");
+                if(fs.existsSync("old-favicon.ico")) fs.unlinkSync("old-favicon.ico");
+
+            }).catch(error => {
+                throw error;
+            });
+        }
+    });
 
 } else {
     throw Error("Couldn't find favicon.png");
