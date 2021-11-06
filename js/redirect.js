42c136088de41690dbd7cb48694f3ed865e4fdfb
Sorp <botsorp@gmail.com>
2021-11-06 05:30:55 +0100

    Page redesign

--- js/redirect.js
+++ js/redirect.js
@@ -9,42 +9,35 @@ function checkForRedirect(p) {
   var redirect;
 
   Object.keys(REDIRECT_DATA).forEach((trigger, index, array) => {
-
     debug("Comparing", p, "and", trigger, " INDEX", index);
 
     if (trigger.includes(",")) {
-
       debug("Doing alias check for", trigger, " INDEX", index);
 
       var triggerSplit = trigger.split(",");
 
       triggerSplit.forEach((t) => {
-
         t = t.trim();
 
-        debug("Alias comparing", p, "and", t)
+        debug("Alias comparing", p, "and", t);
 
         if (p === t) {
           debug("Set redirect to", REDIRECT_DATA[array[index]]);
           redirect = REDIRECT_DATA[array[index]];
         }
-
       });
-
     } else {
       if (p === trigger) {
         debug("Set redirect to", REDIRECT_DATA[array[index]]);
         redirect = REDIRECT_DATA[array[index]];
       }
     }
-
   });
 
   debug("redirect=", redirect);
 
   if (redirect !== undefined) return redirect;
   else return false;
-
 }
 
 function executeRedirect(path, noRedirectCallback, parameter) {
@@ -60,35 +53,27 @@ function redirectToNotFound() {
   debug("No redirect found for " + path);
   if (debug_mode) return;
   //Load 404 page
-  window.location.href = "/not_found";
+  window.location.href = "/";
 }
 
 //Load redirect data from /data/redirect.json
 $.getJSON("/data/redirect.json", (data) => {
-
   debug("Successfully got redirect data:");
   console.log(data);
   REDIRECT_DATA = data;
 
   //Execute redirect after recieving the data
   executeRedirect(path.toLowerCase(), () => {
-
     //Check for /.../...
-    var pathSplit = path.split("/")
+    var pathSplit = path.split("/");
     if (pathSplit.length > 2) {
-
       var targetPath = "/" + pathSplit[1]; //Should result in /...
       var preParameter = pathSplit[0].length + pathSplit[1].length + 1;
       var parameter = path.substr(preParameter, path.length); //Should result in [/...]/...
 
       executeRedirect(targetPath.toLowerCase(), redirectToNotFound, parameter);
-
     } else {
-
       redirectToNotFound();
-
     }
-
   });
-
 });
--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-if (location.protocol != 'https:' && location.hostname != "localhost") location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+if (location.protocol != "https:" && location.hostname != "localhost")
+  location.href =
+    "https:" + window.location.href.substring(window.location.protocol.length);
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    M.Tooltip.init(elems, {});
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".tooltipped");
+  M.Tooltip.init(elems, {});
+});
+
+function clickItem() {
+  if (this.dataset.href) {
+    window.location.href = this.dataset.href;
+  }
+}
+
+document.querySelectorAll(".links > img").forEach((element) => {
+  element.addEventListener("click", clickItem);
 });
