7cf26f4485bcf1dd6970c47c15bc45b5e529485c
Sorp <botsorp@gmail.com>
2021-11-08 21:48:01 +0100

    Change image size on smaller screens to prevent bar going into two lines

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 if (location.protocol != "https:" && location.hostname != "localhost") location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 
+const IconSize = 64;
+
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".tooltipped");
   M.Tooltip.init(elems, {});
-  changeTextColor();
+  responsiveChange();
 });
 
-window.onresize = changeTextColor;
+window.onresize = responsiveChange;
 
-function changeTextColor() {
+function responsiveChange() {
   var element = document.getElementById("sorp-text");
+  //Change text color for better visuality
   if (window.innerWidth < 1550) {
     element.style.color = "white";
   } else {
     element.style.color = "black";
   }
+
+  //Change size of icons
+  // width > 325 = 1
+  // width < 325 = / 2
+  // width < 230 = / 4
+  var factor = window.innerWidth < 325 ? (window.innerWidth < 230 ? 4 : 2) : 1;
+  document.querySelectorAll("img").forEach((img) => {
+    img.width = IconSize / factor;
+    img.height = IconSize / factor;
+  });
 }
 
 var hasClicked = false;
